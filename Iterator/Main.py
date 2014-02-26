370c78fde20058dd0ff757e8fce9f8686ea21a2e
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2014-02-26 17:05:07 +0900

    modified main

--- Iterator/Main.py
+++ Iterator/Main.py
@@ -1,10 +1,13 @@
 __author__ = 'yokoi-h'
 
 from BookShelf import BookShelf
+from BookShelfArray import BookShelfArray
 from Book import Book
 
 if __name__ == '__main__':
-    bookShelf = BookShelf(4)
+    maxSize = 2
+    print "maxSize : ", maxSize
+    bookShelf = BookShelfArray(maxSize)
     bookShelf.append(Book("Around the World in 80 days"))
     bookShelf.append(Book("Bible"))
     bookShelf.append(Book("Cinderella"))
